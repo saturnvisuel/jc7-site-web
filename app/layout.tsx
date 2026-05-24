7b5df185582eaae8ef9a087e53476da63276a887
OsloStudio <saturn.visuel@gmail.com>
2026-05-24 17:13:59 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { Footer } from "@/components/footer";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "JC7 - Judo Courneuvien 7",
@@ -17,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body className={`${inter.className} flex flex-col min-h-screen`}>
+      <body className="font-sans flex flex-col min-h-screen">
         <main className="flex-1">{children}</main>
         <Footer />
       </body>
